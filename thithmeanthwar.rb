71123bd964dfd7e70dfef8b94a77524b7ab86dee
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-09 15:32:29 -0700

    Grammar update

--- thithmeanthwar.rb
+++ thithmeanthwar.rb
@@ -11,7 +11,7 @@ user_input.downcase!
 if user_input.include? "s"
     user_input.gsub!(/s/, "th")
 else
-    puts "There are no \"s\" in your string..."
+    puts "There are no \"s\" in your input..."
 end
 
 puts "Here is your daffified text... #{user_input}"
